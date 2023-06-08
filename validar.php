b4e068f51f19826601299c792af49a938a933ebe
Jorge Simoes <jsimoes@pentafinanciero.cl>
2023-06-08 13:07:00 -0400

    Se agrega plantilla de login e inventario con primera logica de login

---
+++ validar.php
@@ -0,0 +1,15 @@
+<?php
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    echo nl2br("username $username \n");
+    echo nl2br("password $password");
+
+    if ($username === 'admin' && $password === "123"){
+        header("Location: inventario.html");
+        die;
+    } else {
+        header("Location: login.html");
+        die;
+    }
+?>
