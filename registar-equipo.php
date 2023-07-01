476f3cbe9e6551c20b3340482fadadaca5f83bd5
Jorge Simoes <jsimoes@pentafinanciero.cl>
2023-07-01 14:00:33 -0400

    asignacion:  registro de equipo

---
+++ registar-equipo.php
@@ -0,0 +1,14 @@
+<?php
+// validar que el usuario este logeado
+
+?>
+
+<h1>Formlario de registro de equipo</h1>
+
+<form action="validar-equipo.php">
+    <label for="">CBN</label>
+    <input type="number">
+
+    <input type="submit" value="Registrar equipo">
+</form>
+
---
+++ validar-equipo.php
@@ -0,0 +1,6 @@
+<?php
+// validar que el usuario este logeado
+
+// hacer la query de insercion del equipo en la base de datos
+
+// retornar a la vista de inventario
